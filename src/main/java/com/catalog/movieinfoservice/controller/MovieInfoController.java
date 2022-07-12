32a8327ecd77e643252d15c47b6457e86ddd991a
Sonia05 <soniasangwan.cse@gmail.com>
2022-07-12 10:59:51 +1000

    Initial commit

---
+++ src/main/java/com/catalog/movieinfoservice/controller/MovieInfoController.java
@@ -0,0 +1,17 @@
+package com.catalog.movieinfoservice.controller;
+
+import com.catalog.movieinfoservice.domain.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInfoController {
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId, "TestName");
+    }
+}
---
+++ src/main/java/com/catalog/movieinfoservice/domain/Movie.java
@@ -0,0 +1,14 @@
+package com.catalog.movieinfoservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Movie {
+
+    private String movieId;
+    private String name;
+}
